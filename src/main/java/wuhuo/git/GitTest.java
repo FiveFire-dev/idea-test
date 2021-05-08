6483cfa896b78970cd7f4bf2dddb25772760db57
wuhuo <wuhuo@qq.com>
2021-05-08 15:21:53 +0800

    gitidea-test03

--- src/main/java/wuhuo/git/GitTest.java
+++ src/main/java/wuhuo/git/GitTest.java
@@ -3,5 +3,6 @@ package wuhuo.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git-idea-test-02");
+        System.out.println("git-idea-test-03");
     }
 }
