df158eefc0fce5e734516c51be5580c534625e57
wuhuo <wuhuo@qq.com>
2021-05-08 15:31:34 +0800

    gitidea-merge-test

--- src/main/java/wuhuo/git/GitTest.java
+++ src/main/java/wuhuo/git/GitTest.java
@@ -3,5 +3,7 @@ package wuhuo.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git-idea-test");
+        System.out.println("git-idea-test-hot-fix");
+        System.out.println("git-idea-test");
     }
 }
